53b701c29d1d5170e23448240d836a93f62340aa
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-11-07 15:42:49 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -15,6 +16,22 @@ app.options("*", cors(corsConfig));
 
 
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gekvcup.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Hello SMS!')
 })
