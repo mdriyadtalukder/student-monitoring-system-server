153b7d1a88868f54b674f58f84c4597c9aada798
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-11-10 22:01:23 +0600

    almost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 
